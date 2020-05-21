6a1a00a90601f7016194d300a264b64307e10074
Agustin Cutre <agustingonzalezcutre@gmail.com>
2020-05-21 20:44:27 -0300

    PROYECTO INICIADO

---
+++ ProyectoIntegrador/controladores/controladores.js
@@ -0,0 +1,23 @@
+const controlador = {
+
+    listaGeneros : function (req,res) {
+        res.render('listaGeneros')
+    },
+    favoritos : function (req,res) {
+        res.render('favs')
+    },
+    buscadorAvanzado : function (req,res) {
+        res.render('buscadorAvanzado')
+    },
+    detalleSerie : function (req,res) {
+        res.render('detalleSerie')
+    },
+    buscador : function (req,res) {
+        res.render('buscador')
+    },
+    registrarse : function (req,res) {
+        res.render('registrarse')
+    },
+}
+
+module.exports = controlador;
---
+++ ProyectoIntegrador/routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var controladores = require('../controladores/controladores');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+
+
+router.get("/listaGeneros", controladores.listaGeneros);
+router.get("/favs", controladores.favoritos);
+router.get("/buscadorAvanzado", controladores.buscadorAvanzado);
+router.get("/detalleSerie", controladores.detalleSerie);
+router.get("/buscador", controladores.buscador);
+router.get("/registrarse", controladores.registrarse);
+
+
+
+
+
+
+
+module.exports = router;
